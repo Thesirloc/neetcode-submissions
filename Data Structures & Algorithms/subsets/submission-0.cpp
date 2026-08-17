ae4cc7b42d9ba5a4f71a7356b6c941c7c1b4d801
Sushant Singh <81072008+Thesirloc@users.noreply.github.com>
2026-08-17 12:30:03 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void findAllSubsets(int i, vector<vector<int>>& ans, vector<int>& nums, vector<int>& subs){
+        if(i>=nums.size()){
+            ans.push_back(subs);
+            return;
+        }
+        findAllSubsets(i+1, ans, nums, subs);
+        subs.push_back(nums[i]);
+        findAllSubsets(i+1,ans,nums,subs);
+        subs.pop_back();
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> subs;
+        findAllSubsets(0, ans, nums, subs);
+        return ans;
+    }
+};
